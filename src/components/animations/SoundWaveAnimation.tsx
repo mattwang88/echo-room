e0ff72b4f56a6b902d1c055b59f2a9cfb26a6d74
yunchen wang <yunchenwangGarrus@gmail.com>
2025-06-01 09:50:04 +0000

    make the bars on the left end and right end shorter, if couldn't, make other bars higher

--- src/components/animations/SoundWaveAnimation.tsx
+++ src/components/animations/SoundWaveAnimation.tsx
@@ -13,13 +13,13 @@ interface SoundWaveAnimationProps {
 export function SoundWaveAnimation({ width = 512, height = 256, className, isAnimating = true }: SoundWaveAnimationProps) {
   const baseBarClasses = "bg-primary rounded-full";
   const animationClass = isAnimating ? "animate-sound-wave" : "";
-  const barClasses = cn(baseBarClasses, animationClass, "w-3"); // Bar width is w-3
+  const barClasses = cn(baseBarClasses, animationClass, "w-3");
 
   return (
     <div
       className={cn(
         "flex items-center justify-center",
-        "gap-2", // Gap between bars is gap-2
+        "gap-2", 
         className
       )}
       style={{ width: `${width}px`, height: `${height}px` }}
@@ -27,17 +27,19 @@ export function SoundWaveAnimation({ width = 512, height = 256, className, isAni
       data-ai-hint="soundwave animation"
     >
       {/* Symmetrical "W" pattern with 11 bars: L, L, M, H, M, L (center), M, H, M, L, L */}
-      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '0ms'}}></div>    {/* Bar 1 (New) */}
+      {/* Adjusted heights: peaks are h-full, intermediate are h-3/5, lowest are h-1/5 */}
+      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '0ms'}}></div>    {/* Bar 1 (Outer Left) */}
       <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '100ms'}}></div>   {/* Bar 2 */}
-      <div className={cn(barClasses, "h-2/5")} style={{animationDelay: '200ms'}}></div>   {/* Bar 3 */}
-      <div className={cn(barClasses, "h-3/5")} style={{animationDelay: '300ms'}}></div>   {/* Bar 4 - Peak 1 */}
-      <div className={cn(barClasses, "h-2/5")} style={{animationDelay: '400ms'}}></div>   {/* Bar 5 */}
-      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '500ms'}}></div>   {/* Bar 6 - Central Valley */}
-      <div className={cn(barClasses, "h-2/5")} style={{animationDelay: '600ms'}}></div>   {/* Bar 7 (Mirror of Bar 5) */}
-      <div className={cn(barClasses, "h-3/5")} style={{animationDelay: '700ms'}}></div>   {/* Bar 8 (Mirror of Bar 4 - Peak 2) */}
-      <div className={cn(barClasses, "h-2/5")} style={{animationDelay: '800ms'}}></div>   {/* Bar 9 (Mirror of Bar 3) */}
-      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '900ms'}}></div>   {/* Bar 10 (Mirror of Bar 2) */}
-      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '1000ms'}}></div>  {/* Bar 11 (New) */}
+      <div className={cn(barClasses, "h-3/5")} style={{animationDelay: '200ms'}}></div>   {/* Bar 3 (Rising - Taller) */}
+      <div className={cn(barClasses, "h-full")} style={{animationDelay: '300ms'}}></div>   {/* Bar 4 - Peak 1 (Tallest) */}
+      <div className={cn(barClasses, "h-3/5")} style={{animationDelay: '400ms'}}></div>   {/* Bar 5 (Falling - Taller) */}
+      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '500ms'}}></div>   {/* Bar 6 - Central Valley (Lowest) */}
+      <div className={cn(barClasses, "h-3/5")} style={{animationDelay: '600ms'}}></div>   {/* Bar 7 (Rising - Taller) */}
+      <div className={cn(barClasses, "h-full")} style={{animationDelay: '700ms'}}></div>   {/* Bar 8 - Peak 2 (Tallest) */}
+      <div className={cn(barClasses, "h-3/5")} style={{animationDelay: '800ms'}}></div>   {/* Bar 9 (Falling - Taller) */}
+      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '900ms'}}></div>   {/* Bar 10 */}
+      <div className={cn(barClasses, "h-1/5")} style={{animationDelay: '1000ms'}}></div>  {/* Bar 11 (Outer Right) */}
     </div>
   );
 }
+
